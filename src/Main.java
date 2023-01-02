b7d209b89aeb98d34cf8a2cf4e3842b3eb4d50d9
Shahnia <khanshahnia@gmail.com>
2023-01-02 18:44:48 -0500

    Project3

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Account tim = new Account("H Sarkr", "A00001");
+
+
+        Account beyonce = new Account("Khan", "B00002");
+        beyonce.showMenu();
+
+
+    }
+
+
+
+        //System.out.println("Hello world!");
+    }
